b31d2a89ece594a57016b39f5a893e92453cd0b9
salazarkendall <kendallsalazarvargas@gmail.com>
2022-02-25 18:25:35 -0600

    build test: 4

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomeScreen } from '../components/HomeScreen';
 import { LoginScreen } from '../components/LoginScreen';
 import { MovieDetailScreen } from '../components/MovieDetailScreen';
